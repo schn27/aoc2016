f8664c4b96447bda6787b5b5710252efd9b31c32
schn27 <schn27@gmail.com>
2017-01-09 00:22:44 +0300

    refactor level1

--- 01.js
+++ 01.js
@@ -12,7 +12,7 @@ function calc() {
 		heading = turn(heading, command.substring(0, 1));
 		
 		for (var i = parseInt(command.substring(1)); i > 0; --i) {
-			coords = move(coords, heading, 1);
+			coords = move(coords, heading);
 			
 			if (bunnyCoords == null) {
 				var key = coords.join(":");
@@ -50,25 +50,9 @@ function turn(heading, turn) {
 	return newHeading;
 }
 
-function move(coords, heading, distance) {
-	var newCoords = coords.slice();
-
-	switch (heading) {
-	case 0:
-		newCoords[1] += distance;
-		break;
-	case 1:
-		newCoords[0] += distance;
-		break;
-	case 2:
-		newCoords[1] -= distance;
-		break;
-	case 3:
-		newCoords[0] -= distance;
-		break;
-	}
-
-	return newCoords;
+function move(coords, heading) {
+	var moves = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+	return [coords[0] + moves[heading][0], coords[1] + moves[heading][1]];
 }
 
 var input = "R2, L1, R2, R1, R1, L3, R3, L5, L5, L2, L1, R4, R1, R3, L5, L5, R3, L4, L4, R5, R4, R3, L1, L2, R5, R4, L2, R1, R4, R4, L2, L1, L1, R190, R3, L4, R52, R5, R3, L5, R3, R2, R1, L5, L5, L4, R2, L3, R3, L1, L3, R5, L3, L4, R3, R77, R3, L2, R189, R4, R2, L2, R2, L1, R5, R4, R4, R2, L2, L2, L5, L1, R1, R2, L3, L4, L5, R1, L1, L2, L2, R2, L3, R3, L4, L1, L5, L4, L4, R3, R5, L2, R4, R5, R3, L2, L2, L4, L2, R2, L5, L4, R3, R1, L2, R2, R4, L1, L4, L4, L2, R2, L4, L1, L1, R4, L1, L3, L2, L2, L5, R5, R2, R5, L1, L5, R2, R4, R4, L2, R5, L5, R5, R5, L4, R2, R1, R1, R3, L3, L3, L4, L3, L2, L2, L2, R2, L1, L3, R2, R5, R5, L4, R3, L3, L4, R2, L5, R5";
